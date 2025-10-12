b1f1694988c0e2fc12031a6350aa3ce4a86942f7
tonnahe171051 <tonnahe171051@gmail.com>
2025-10-12 23:47:48 +0700

    feature: model for get fargo rating feature

---
+++ PoolMateBackend/Integrations/FargoRate/Models/FargoSearchResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace PoolMate.Api.Integrations.FargoRate.Models
+{
+    public class FargoSearchResponse
+    {
+        public List<FargoPlayerData> Value { get; set; } = new List<FargoPlayerData>();
+    }
+
+    public class FargoPlayerData
+    {
+        public string? Id { get; set; }
+        public string? ReadableId { get; set; }
+        public string? MembershipId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Location { get; set; }
+        public string? Rating { get; set; }
+        public string? Robustness { get; set; }
+        public string? ProvisionalRating { get; set; }
+        public string? EffectiveRating { get; set; }
+        public int? MembershipNumber { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ImsId { get; set; }
+        public string? ShareMatches { get; set; }
+        public string? StatsOverall { get; set; }
+        public string? StatsByRating { get; set; }
+        public string? RatingHistory { get; set; }
+    }
+}
---
+++ PoolMateBackend/Integrations/FargoRate/Models/PlayerFargoSearchResult.cs
@@ -0,0 +1,11 @@
+﻿namespace PoolMate.Api.Integrations.FargoRate.Models
+{
+    public class PlayerFargoSearchResult
+    {
+        public int TournamentPlayerId { get; set; }
+        public string OriginalName { get; set; } = string.Empty;
+        public FargoPlayerResult? FargoResult { get; set; }
+        public bool IsFound => FargoResult != null;
+        public string? ErrorMessage { get; set; }
+    }
+}
