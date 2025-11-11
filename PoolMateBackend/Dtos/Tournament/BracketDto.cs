6fe183e518f984afdd37a61283fcdd256860201e
tonnahe171051 <tonnahe171051@gmail.com>
2025-11-11 23:43:03 +0700

    feature: bonus some info for bracket

--- PoolMateBackend/Dtos/Tournament/BracketDto.cs
+++ PoolMateBackend/Dtos/Tournament/BracketDto.cs
@@ -37,18 +37,24 @@ namespace PoolMate.Api.Dtos.Tournament
         public int PositionInRound { get; set; }
         public BracketSide Bracket { get; set; }
         public MatchStatus Status { get; set; }
-        
+
         public PlayerDto? Player1 { get; set; }
         public PlayerDto? Player2 { get; set; }
         public PlayerDto? Winner { get; set; }
         
+        public DateTime? ScheduledUtc { get; set; }
+        public string? ScheduledDisplay { get; set; } // "Oct 21, 11:00h"
+
+        public int? TableId { get; set; }
+        public string? TableLabel { get; set; }
+
         public int? ScoreP1 { get; set; }
         public int? ScoreP2 { get; set; }
         public int? RaceTo { get; set; }
-        
+
         public int? NextWinnerMatchId { get; set; }
         public int? NextLoserMatchId { get; set; }
-        
+
         public bool IsBye { get; set; } //handle BYE matches
     }
 
@@ -57,5 +63,7 @@ namespace PoolMate.Api.Dtos.Tournament
         public int TpId { get; set; }
         public string Name { get; set; } = default!;
         public int? Seed { get; set; }
+        public string? Country { get; set; } 
+        public int? FargoRating { get; set; }
     }
 }
